da776a9b030a71c8e0babeb69e36e4bdf01cc60c
SAK <SAK@gmail.com>
2025-11-19 17:00:40 -0500

    Add/Update places modal fixed, api calls fixed, UI design updated, DB Schema Updated

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://spotnere-admin-dashboard.vercel.app/",
+        "https://spotnere-admin-dashboard.vercel.app",
         "http://localhost:8080",
         "http://localhost:5173",
         "http://127.0.0.1:8080",
